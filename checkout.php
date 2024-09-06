a955e65ccfeda39890bf31eb7eb8cbc9ced5d7f6
adnane-ka <dannykadri0@gmail.com>
2024-09-06 12:24:22 +0100

    Deleting files from local repo

--- checkout.php
+++
@@ -1,6 +0,0 @@
-<form action="<?php echo urldecode($_GET['completeUrl']); ?>" method="post" style="display: flex; flex-direction: column; width:30%;">
-    <img src='<?php echo urldecode($_GET['qrSrc']); ?>'>
-
-    <input type="hidden" name="transactionId" value="<?php echo $_GET['transactionId']; ?>">
-    <button type="submit">I Paid, Complete My Order.</button>
-</form>
